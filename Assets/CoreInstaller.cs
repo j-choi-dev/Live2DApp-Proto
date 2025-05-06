bce446371441fb4dd810aebc728ff297aa63e53b
choi <choi@cover-corp.com>
2025-05-06 12:07:34 +0900

    통합작업 실시

---
+++ Assets/CoreInstaller.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace AppCore.Installer
+{
+    public class CoreInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
